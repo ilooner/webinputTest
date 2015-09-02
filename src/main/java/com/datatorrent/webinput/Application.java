e86ff0abfbe21d5a69a509fe09d313d7ab258022
Timothy Farkas <tim@datatorrent.com>
2015-09-02 16:23:34 -0700

    Initial commit.

---
+++ src/main/java/com/datatorrent/webinput/Application.java
@@ -0,0 +1,25 @@
+/**
+ * Put your copyright and license info here.
+ */
+package com.datatorrent.webinput;
+
+import com.datatorrent.api.DAG;
+import com.datatorrent.api.DAG.Locality;
+import com.datatorrent.api.StreamingApplication;
+import com.datatorrent.api.annotation.ApplicationAnnotation;
+import com.datatorrent.lib.io.ConsoleOutputOperator;
+import com.datatorrent.lib.io.PubSubWebSocketAppDataQuery;
+import org.apache.hadoop.conf.Configuration;
+
+@ApplicationAnnotation(name="MyFirstApplication")
+public class Application implements StreamingApplication
+{
+
+  @Override
+  public void populateDAG(DAG dag, Configuration conf)
+  {
+    PubSubWebSocketAppDataQuery pubSub = dag.addOperator("webSocketAppDataQuery", PubSubWebSocketAppDataQuery.class);
+    ConsoleOutputOperator cons = dag.addOperator("console", new ConsoleOutputOperator());
+    dag.addStream("randomData", pubSub.outputPort, cons.input);
+  }
+}
